8ef4e4ef5d8084fa0d9a591c8798d447f62de60f
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2021-05-13 08:16:13 +0200

    feat: use new class name

--- LinkedIn_Connect.js
+++ LinkedIn_Connect.js
@@ -1,5 +1,4 @@
 var c = 1;
 
-document.querySelectorAll('button[data-control-name="people_connect"]').forEach (function(r){ setTimeout(function(){ c = c +1 ; r.click();} , 1500 * c); });
-
-window.loaction.reload();
+document.querySelectorAll('button[class="full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view"]').forEach (function(r){ setTimeout(function(){ c = c +1 ; r.click();} , 1500 * c); });
+// window.loaction.reload();
