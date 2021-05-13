e825325c5e2b69372a1ad2757312fd8594873a67
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2021-05-13 07:38:04 +0200

    Create LinkedIn_Connect.js

---
+++ LinkedIn_Connect.js
@@ -0,0 +1,5 @@
+var c = 1;
+
+document.querySelectorAll('button[data-control-name="people_connect"]').forEach (function(r){ setTimeout(function(){ c = c +1 ; r.click();} , 1500 * c); });
+
+window.loaction.reload();
