a5910da9393ec51298cfe80f196d2116fc27529e
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2021-05-13 07:31:30 +0200

    Update Instagram_unfollow.js

--- Instagram_unfollow.js
+++ Instagram_unfollow.js
@@ -1,8 +1,8 @@
   // node list based on class `sqdOP  L3NKy    _8A5w5    `
   const unfollowBtns = document.querySelectorAll('[class="sqdOP  L3NKy    _8A5w5    "]');
 
-  //convert node list to array using es6 spread operator to can loop on it
-  [...unfollowBtns].forEach((el) => {
+  // iterate over buttons then perform click()
+  unfollowBtns.forEach((el) => {
     el.click();
     let confirmUnfollowBtn = document.querySelector(
       '[class="aOOlW -Cab_   "]'
