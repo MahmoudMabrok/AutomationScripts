319a0f2422675b1f206c62eee317d8498ea86990
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2021-05-13 13:51:45 +0200

    docs: add steps

--- LinkedIn_Connect.js
+++ LinkedIn_Connect.js
@@ -1,4 +1,12 @@
+  /*
+  
+  Steps: 
+  - open network window from Linkedin 
+  - open console(click F12 on chrome)
+  - paste code 
+  
+  
+  */
 var c = 1;
-
 document.querySelectorAll('button[class="full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view"]').forEach (function(r){ setTimeout(function(){ c = c +1 ; r.click();} , 1500 * c); });
 // window.loaction.reload();
