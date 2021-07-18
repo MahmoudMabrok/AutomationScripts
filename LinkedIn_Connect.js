12b414ee7eec683237f3fd63bdf5f77edb17f665
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2021-07-18 18:05:49 +0200

    Update LinkedIn_Connect.js

--- LinkedIn_Connect.js
+++ LinkedIn_Connect.js
@@ -7,6 +7,12 @@
   
   
   */
-var c = 1;
-document.querySelectorAll('button[class="full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view"]').forEach (function(r){ setTimeout(function(){ c = c +1 ; r.click();} , 1500 * c); });
+// var c = 1;
+//document.querySelectorAll('button[class="full-width artdeco-button artdeco-button--2 artdeco-button--full artdeco-button--secondary ember-view"]').forEach (function(r){ setTimeout(function(){ c = c +1 ; r.click();} , 1500 * c); });
 // window.loaction.reload();
+
+var c = 1;
+Array.from(document.querySelectorAll("span.artdeco-button__text")).filter(e =>  e.innerText == "Connect").forEach(e => {setTimeout(function(){ c = c +1 ; e.click();} , 1500 * c);})
+
+
+
